0bd2a91e26ca4664df8565f53bfa94b2ec70b770
evans <evans.joewono@gmail.com>
2022-03-01 20:57:45 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-eqipv="X-UA-Compatible" content="IE=Edge">
+    <title>Belajar</title>
+</head>
+<body>
+    <h1>Yo</h1>
+    <?php
+        $varBaru = "Evans";
+        $var = "12223";
+        $var1 = 12223;
+        $var2 = 1221;
+    ?>
+    <?php echo "Hello, nama saya ".$varBaru."<br>"; ?>
+    <?php echo "\r\nHello $var"."<br>"; ?>
+    <?php echo "$var1 - $var2 = ".($var1 - $var2); ?>
+</body>
+</html>
